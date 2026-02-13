5658711d4b374b6ec92f9af31f150c6d79fa6e13
Hugo Márquez Diestra <95510299+HugoX2003@users.noreply.github.com>
2026-02-13 02:55:41 -0500

    day13: Add day5 EKS VPC Terraform config; move day3 files
    Rename existing top-level Terraform files into infra/terraform/day3-k8s and add a new infra/terraform/day5-eks-vpc workspace. The new day5 folder includes terraform.tf (required provider constraints), provider.tf (AWS region), vpc.tf (terraform-aws-modules/vpc to create VPC, subnets, and NAT), eks.tf (terraform-aws-modules/eks to provision an EKS cluster v1.30 with two managed node groups), and a generated .terraform.lock.hcl. This adds a standalone VPC + EKS setup while organizing the previous day3 K8s configs into their own directory.

---
+++ infra/terraform/day5-eks-vpc/eks.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"
+
+  cluster_name    = "terraform-eks-cluster"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    ng1 = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.small"]
+      subnet_ids     = module.vpc.private_subnets
+    }
+
+    ng2 = {
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      instance_types = ["t3.small"]
+      subnet_ids     = module.vpc.private_subnets
+    }
+  }
+}
